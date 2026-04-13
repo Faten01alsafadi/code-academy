287c7a5f6cf42058f7d449f72c239a3d7f2f24d8
faten <fatensafad@gmail.com>
2026-04-13 18:26:18 +0300

    fix

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -1,16 +1,60 @@
 import { useParams } from "react-router-dom";
+import students from "../data/student";
+
 
 function Verify() {
   const { id } = useParams();
+  const student = students[id];
 
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h2>Certificate Verification</h2>
-      <p>ID: {id}</p>
+    <div
+      style={{
+        textAlign: "center",
+        padding: "60px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h2> Certificate Verification</h2>
 
-      <h3 style={{ color: "green" }}>
-        ✔ Valid Certificate
-      </h3>
+      <div
+        style={{
+          marginTop: "30px",
+          padding: "20px",
+          borderRadius: "12px",
+          display: "inline-block",
+          minWidth: "300px",
+          boxShadow: "0 5px 20px rgba(0,0,0,0.1)",
+          background: "white",
+        }}
+      >
+        {student ? (
+          <>
+            <h3 style={{ marginBottom: "10px" }}>{student.name}</h3>
+
+            <p style={{ color: "#555" }}>
+              {student.track} Developer
+            </p>
+
+            <p style={{ fontSize: "12px", opacity: 0.7 }}>
+              ID: {id}
+            </p>
+
+            <h3 style={{ color: "green", marginTop: "15px" }}>
+              ✔ Valid Certificate
+            </h3>
+          </>
+        ) : (
+          <>
+            <h3 style={{ color: "red" }}>
+              ❌ Invalid Certificate
+            </h3>
+
+            <p style={{ fontSize: "12px", opacity: 0.7 }}>
+              This certificate does not exist in our system
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data/student.js
@@ -0,0 +1,47 @@
+const students = {
+  // FRONT-END
+  "FE-2026-001": {
+    name: "Fajer Jbaee",
+    track: "Front-End",
+  },
+  "FE-2026-002": {
+    name: "Luna Alsafadi",
+    track: "Front-End",
+  },
+  "FE-2026-003": {
+    name: "Lara Alhajali",
+    track: "Front-End",
+  },
+  "FE-2026-004": {
+    name: "Atheer Maklad",
+    track: "Front-End",
+  },
+  "FE-2026-005": {
+    name: "Asala Alghadban",
+    track: "Front-End",
+  },
+  "FE-2026-006": {
+    name: "Taim Quroni",
+    track: "Front-End",
+  },
+
+  // BACK-END
+  "BE-2026-001": {
+    name: "Zain Ghabra",
+    track: "Back-End",
+  },
+  "BE-2026-002": {
+    name: "Ashraf Hlal",
+    track: "Back-End",
+  },
+  "BE-2026-003": {
+    name: "Hadeel Zidan",
+    track: "Back-End",
+  },
+  "BE-2026-004": {
+    name: "Jana Abo Omar",
+    track: "Back-End",
+  },
+};
+
+export default students;
